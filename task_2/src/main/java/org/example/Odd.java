822765b86823e216a27f60da1e4d40a02124c6cf
VolodymyrLutsiv <Vova_96lu@ukr.net>
2020-10-01 22:41:35 +0300

    odd done

--- task_2/src/main/java/org/example/Odd.java
+++ task_2/src/main/java/org/example/Odd.java
@@ -5,8 +5,10 @@ public class Odd {
     public static void main(String[] args) {
         isOdd(0);
     }
+          public static boolean isOdd ( int i){
+            if ( i % 2==0)    {
+        return false;}
+            else {return true;}
 
-    public static boolean isOdd(int i) {
-        return false;
     }
 }
