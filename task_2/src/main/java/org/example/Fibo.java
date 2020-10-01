b0dd7edb245137232183c25585cc4a211b84478b
VolodymyrLutsiv <Vova_96lu@ukr.net>
2020-10-01 23:17:57 +0300

    odd,fibo done

--- task_2/src/main/java/org/example/Fibo.java
+++ task_2/src/main/java/org/example/Fibo.java
@@ -1,11 +1,23 @@
 package org.example;
 
 public class Fibo {
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
+
+       public static int fibo(int i) {
+              if (i==1 || i==2) {
+                  return 1;}
+              else {
+            int a1, a2, buffer;
+                  a1 = 1; // перше число фібоначі
+                  a2 = 1; // друге число фібоначі
+            for (int j=3;j<=i;j++) {
+              buffer = a2; // зберігаємо останнє відоме значення
+                a2 = a1 + a2; // записуємо нове значення (наступне)/посуваємо комірку
+                a1 = buffer;
+            }
+                  return a2;
+        }
 
     }
 
-    public static int fibo(int i) {
-        return 0;
-    }
 }
