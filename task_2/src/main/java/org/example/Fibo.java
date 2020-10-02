143f6957b133b7e30fc9c86f764650dec3996cf1
VolodymyrLutsiv <Vova_96lu@ukr.net>
2020-10-02 16:37:57 +0300

    fibo done (recursion)

--- task_2/src/main/java/org/example/Fibo.java
+++ task_2/src/main/java/org/example/Fibo.java
@@ -1,23 +1,16 @@
 package org.example;
 
 public class Fibo {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 
-       public static int fibo(int i) {
-              if (i==1 || i==2) {
-                  return 1;}
-              else {
-            int a1, a2, buffer;
-                  a1 = 1; // перше число фібоначі
-                  a2 = 1; // друге число фібоначі
-            for (int j=3;j<=i;j++) {
-              buffer = a2; // зберігаємо останнє відоме значення
-                a2 = a1 + a2; // записуємо нове значення (наступне)/посуваємо комірку
-                a1 = buffer;
-            }
-                  return a2;
-        }
+    public static int fibo(int i) {
+        if (i == 0) return 0;
+        if (i == 1) return 1;
+        return fibo(i - 1) + fibo(i - 2);
 
     }
 
 }
+
+
