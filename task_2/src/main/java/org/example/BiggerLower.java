9a645218915d181ccd494d40edca10937092d288
Oleg Brynyarskyy <obrynyarskyy@intellias.com>
2020-10-02 14:43:26 +0300

    BiggerLower task

---
+++ task_2/src/main/java/org/example/BiggerLower.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class BiggerLower {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * Method make decision if i is bigger that j
+     */
+    public static boolean isBigger(int i, int j) {
+        return false;
+    }
+
+    /*
+     * Method make decision if i is lower that j
+     */
+    public static boolean isLower(int a, int b) {
+        return false;
+    }
+}
