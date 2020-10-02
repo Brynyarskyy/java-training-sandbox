8da8721bc1e2fd9540efa8f16fd7ff9cee7f5f94
VolodymyrLutsiv <Vova_96lu@ukr.net>
2020-10-02 17:48:45 +0300

    test 2

---
+++ task_2/src/main/java/org/example/BiggerLower.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class BiggerLower {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * Method make decision if i is bigger that j
+     */
+    public static boolean isBigger(int i, int j) {
+
+        if (i>j);
+        return true;
+
+    }
+
+    /*
+     * Method make decision if i is lower that j
+     */
+    public static boolean isLower(int i, int j) {
+       !isBigger();
+        return false;
+    }
+}
