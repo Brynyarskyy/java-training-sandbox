3ae2e0fdcf69d61577e5bf5e1044cfd15c1f82fa
VolodymyrLutsiv <Vova_96lu@ukr.net>
2020-10-04 14:53:06 +0300

    Sequence done +-

--- task_2/src/main/java/org/example/Sequence.java
+++ task_2/src/main/java/org/example/Sequence.java
@@ -6,10 +6,17 @@ public class Sequence {
     }
 
     public static boolean isValid(int[] sequence) {
-        for (int i = 0; i < sequence.length-1; i++)
-            if (sequence[i] > sequence[i + 1]) {
+        for (int i = 0; i < sequence.length - 1; i++) {
+            if (isBigger(sequence[i], sequence[i + 1])) {
                 return false;
             }
+        }
         return true;
+    }
+
+    public static boolean isBigger(int i, int j) {
+        return  (i > j);
+
+
     }
 }
